027c378eb1318e02b3efec0b053fe1cd705864ea
Rafal Nowosielski <rnowosielski@cimpress.com>
2017-12-07 15:36:06 +0100

    Provide both deafult export and named export

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ export class Comments extends _Comments {
 
 }
 
+export class CommentsDrawerLink extends _CommentsDrawerLink {
+
+}
+
 export default class CommentsDrawerLink extends _CommentsDrawerLink {
 
 }
