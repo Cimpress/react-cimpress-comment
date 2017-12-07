0d51e1c7bd8469d00697372cafa0362ac9d048f0
Rafal Nowosielski <rnowosielski@cimpress.com>
2017-12-07 15:28:59 +0100

    Fixes to make inline block relevant, and provide default export.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import _Comments from './Comments';
 import _CommentsDrawerLink from './CommentsDrawerLink';
 
-export default class Comments extends _Comments {
+export class Comments extends _Comments {
 
 }
 
-export class CommentsDrawerLink extends _CommentsDrawerLink {
+export default class CommentsDrawerLink extends _CommentsDrawerLink {
 
 }
