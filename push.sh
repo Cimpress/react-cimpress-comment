063472403a539b0f7a01b80aba9820c39013a794
Rafal Nowosielski <rnowosielski@cimpress.com>
2018-01-29 23:15:55 +0100

    Fix the bash script to push results

--- push.sh
+++ push.sh
@@ -2,11 +2,10 @@
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git remote add origin-pages https://${PUSH_BACK}@github.com/Cimpress/react-cimpress-comment.wiki.git > /dev/null 2>&1
-git fetch origin-pages
-git checkout -b origin-pages/master
-git reset --hard origin-pages/master
+git remote add origin-results https://${PUSH_BACK}@github.com/Cimpress/react-cimpress-comment.git > /dev/null 2>&1
+git checkout -b results
+git pull origin-results/results
 git add ./backstop_data/bitmaps_test/* -f
 git add ./backstop_data/html_report/* -f
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-git push --quiet --set-upstream origin-pages gh-pages
+git push --quiet --set-upstream origin-results results
