6dd0c90c0a4d8753473a41d338a064e9c67d993b
Rafal Nowosielski <rnowosielski@cimpress.com>
2018-01-30 00:04:26 +0100

    Pull changes.

--- push.sh
+++ push.sh
@@ -5,9 +5,10 @@ git config --global user.name "Travis CI"
 git remote add origin-results https://${PUSH_BACK}@github.com/Cimpress/react-cimpress-comment.git > /dev/null 2>&1
 git fetch --all
 git checkout -b results
-git reset --hard origin-results/results
 git add ./backstop_data/bitmaps_test/* -f
 git add ./backstop_data/html_report/* -f
 git add ./backstop_data/bitmaps_reference/* -f
+git clean -n
+git pull origin results -f
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git push --quiet --set-upstream origin-results results
