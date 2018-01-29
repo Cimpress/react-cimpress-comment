fb0d169ed8c89c4d69e59a24c83a59d5d8bd5fee
Travis CI <travis@travis-ci.org>
2018-01-29 18:41:24 +0100

    Update env vars.

--- push.sh
+++ push.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
-
-commit_website_files() {
-  git checkout -b gh-pages
-  git add ./backstop_data/bitmaps_test/* -f
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
-  git remote add origin-pages https://${PUSH_BACK}@github.com/Cimpress/react-cimpress-comment > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages
-}
-
-setup_git
-commit_website_files
-upload_files
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git remote add origin-pages https://${PUSH_BACK}@github.com/Cimpress/react-cimpress-comment > /dev/null 2>&1
+git fetch origin-page
+git reset --hard origin-page/gh-pages
+git checkout -b gh-pages
+git add ./backstop_data/bitmaps_test/* -f
+git add ./backstop_data/html_report/* -f
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git push --quiet --set-upstream origin-pages gh-pages
