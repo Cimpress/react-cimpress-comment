785fd2e86b74944b0f846e5113ed417a23108779
Rafal Nowosielski <rnowosielski@cimpress.com>
2018-01-29 23:37:51 +0100

    Pull changes.

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git remote add origin-results https://${PUSH_BACK}@github.com/Cimpress/react-cimpress-comment.git > /dev/null 2>&1
 git checkout -b results
-git pull origin-results/results
+git pull origin-results results
 git add ./backstop_data/bitmaps_test/* -f
 git add ./backstop_data/html_report/* -f
 git add ./backstop_data/bitmaps_reference/* -f
