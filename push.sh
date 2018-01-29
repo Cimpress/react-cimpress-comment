cd5248050090d9dba98645278aaf75d51f047c50
Rafal Nowosielski <rnowosielski@cimpress.com>
2018-01-30 00:22:29 +0100

    Pull changes.

--- push.sh
+++ push.sh
@@ -10,5 +10,6 @@ git add ./backstop_data/html_report/* -f
 git add ./backstop_data/bitmaps_reference/* -f
 git clean -f
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-git pull origin results -f
+git merge --strategy-option ours origin/results
 git push --quiet --set-upstream origin-results results
+
