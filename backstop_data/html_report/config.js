b732df923319ea5ed5138ac3c3ed0bdc1ad5de27
Travis CI <travis@travis-ci.org>
2018-01-29 23:57:40 +0000

    Travis build: 117

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,169 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/cimpress-react-comment_foreign_comments_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180129-235717/cimpress-react-comment_foreign_comments_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "cimpress-react-comment_foreign_comments_0_document_0_phone.png",
+        "label": "foreign_comments",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 3,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/cimpress-react-comment_foreign_comments_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180129-235717/cimpress-react-comment_foreign_comments_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "cimpress-react-comment_foreign_comments_0_document_1_tablet.png",
+        "label": "foreign_comments",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 3,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.80",
+          "analysisTime": 51
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/cimpress-react-comment_own_comments_with_edit_off_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180129-235717/cimpress-react-comment_own_comments_with_edit_off_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "cimpress-react-comment_own_comments_with_edit_off_0_document_0_phone.png",
+        "label": "own_comments_with_edit_off",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 3,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/cimpress-react-comment_own_comments_with_edit_off_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180129-235717/cimpress-react-comment_own_comments_with_edit_off_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "cimpress-react-comment_own_comments_with_edit_off_0_document_1_tablet.png",
+        "label": "own_comments_with_edit_off",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 3,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.80",
+          "analysisTime": 46
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/cimpress-react-comment_own_comments_with_edit_on_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180129-235717/cimpress-react-comment_own_comments_with_edit_on_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "cimpress-react-comment_own_comments_with_edit_on_0_document_0_phone.png",
+        "label": "own_comments_with_edit_on",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 3,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/cimpress-react-comment_own_comments_with_edit_on_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180129-235717/cimpress-react-comment_own_comments_with_edit_on_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "cimpress-react-comment_own_comments_with_edit_on_0_document_1_tablet.png",
+        "label": "own_comments_with_edit_on",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 3,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.90",
+          "analysisTime": 57
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/cimpress-react-comment_own_comments_with_edit_on_editing_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180129-235717/cimpress-react-comment_own_comments_with_edit_on_editing_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "cimpress-react-comment_own_comments_with_edit_on_editing_0_document_0_phone.png",
+        "label": "own_comments_with_edit_on_editing",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 3,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/cimpress-react-comment_own_comments_with_edit_on_editing_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180129-235717/cimpress-react-comment_own_comments_with_edit_on_editing_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "cimpress-react-comment_own_comments_with_edit_on_editing_0_document_1_tablet.png",
+        "label": "own_comments_with_edit_on_editing",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 3,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.86",
+          "analysisTime": 46
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
