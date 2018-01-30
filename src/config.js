bbd16bd7114964ffc588c51214738ecace2af312
Rafal Nowosielski <rnowosielski@cimpress.com>
2018-01-30 13:28:58 +0100

    Check for nulls in envs.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const COMMENT_SERVICE_URL = 'https://comment.trdlnk.cimpress.io';
-const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || 'https://https://api.cimpress.io';
-const SERVICE_URL = process.env.COMMENT_SERVICE_URL || COMMENT_SERVICE_URL;
+const AUTH_SERVICE_URL = (process && process.env ? process.env.AUTH_SERVICE_URL : null) || 'https://api.cimpress.io';
+const SERVICE_URL = (process && process.env ? process.env.COMMENT_SERVICE_URL : null) || 'https://comment.trdlnk.cimpress.io';
+
 export {
   SERVICE_URL,
   AUTH_SERVICE_URL
