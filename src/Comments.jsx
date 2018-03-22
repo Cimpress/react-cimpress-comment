90370a4a9e0bd3d0d3cff0bb1009b09f7d7f5743
Rafal Nowosielski <rnowosielski@cimpress.com>
2018-03-22 20:35:30 +0100

    Apparently api of the customizr changed in the meantime. I am sure they will fix it to be backwards compatible, but moving to the new api anyway.

--- src/Comments.jsx
+++ src/Comments.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import VisibilitySensor from 'react-visibility-sensor';
-import 'react-placeholder/lib/reactPlaceholder.css';
-import PropTypes from 'prop-types';
-import Comment from './Comment';
-import '../style/index.css';
-import { shapes } from '@cimpress/react-components';
-import { Alert } from '@cimpress/react-components';
+import React, {Component} from 'react'
+import VisibilitySensor from 'react-visibility-sensor'
+import 'react-placeholder/lib/reactPlaceholder.css'
+import PropTypes from 'prop-types'
+import Comment from './Comment'
+import '../style/index.css'
+import {shapes} from '@cimpress/react-components'
+import {Alert} from '@cimpress/react-components'
 
-let {Spinner} = shapes;
+let {Spinner} = shapes
 
-import CommentsClient from './CommentsClient';
-import { SERVICE_URL, CUSTOMIZR_URL } from './config';
-import { MentionsInput, Mention } from 'react-mentions';
-import MentionsClient from './MentionsClient';
+import CommentsClient from './CommentsClient'
+import {SERVICE_URL, CUSTOMIZR_URL} from './config'
+import {MentionsInput, Mention} from 'react-mentions'
+import MentionsClient from './MentionsClient'
 
 export default class _Comments extends React.Component {
 
-  constructor (props) {
-    super(props);
-    this.commentServiceUrl = SERVICE_URL;
-    this.customizrResource = `http://comment.trdlnk.cimpress.io/`
-    this.commentsClient = new CommentsClient(props.accessToken, props.resourceUri);
-    this.mentionsClient = new MentionsClient(props.accessToken);
+  constructor(props) {
+    super(props)
+    this.commentServiceUrl = SERVICE_URL
+    this.customizrResource = encodeURIComponent(`https://comment.trdlnk.cimpress.io/`)
+    this.commentsClient = new CommentsClient(props.accessToken, props.resourceUri)
+    this.mentionsClient = new MentionsClient(props.accessToken)
     this.state = {
       blockEnter: false,
       visible: false,
@@ -31,74 +31,76 @@ export default class _Comments extends React.Component {
       commentToAdd: props.initialValue || '',
       failed: false,
       alertDismissed: true
-    };
+    }
   }
 
-  componentWillMount () {
-    setTimeout(() => this.forceFetchComments(), 10);
+  componentWillMount() {
+    setTimeout(() => this.forceFetchComments(), 10)
   }
 
   componentDidMount() {
-    this._ismounted = true;
-    fetch(`${CUSTOMIZR_URL}/v1/settings?resourceId=${this.customizrResource}`, {
+    this._ismounted = true
+    fetch(`${CUSTOMIZR_URL}/v1/${this.customizrResource}/settings`, {
       method: "GET",
       headers: {
         'Authorization': `Bearer ${this.props.accessToken}`,
       },
     }).then(res => {
-      return res.json();
+      return res.json()
     }).then(json => {
       if (this._ismounted) {
-        this.setState({alertDismissed: json.data
-          && json.data.mentionsUsageNotification
-          && json.data.mentionsUsageNotification.alertDismissed === true})
+        this.setState({
+          alertDismissed: json
+          && json.mentionsUsageNotification
+          && json.mentionsUsageNotification.alertDismissed === true
+        })
       }
     })
   }
 
   componentWillUnmount() {
-    this._ismounted = false;
+    this._ismounted = false
   }
 
-  componentWillReceiveProps (newProps) {
-    clearInterval(this.refreshInterval);
-    this.refreshInterval = setInterval(() => this.forceFetchComments(), Math.max((this.props.refreshInterval || 60) * 1000, 5000));
-    this.commentsClient = new CommentsClient(newProps.accessToken, newProps.resourceUri);
+  componentWillReceiveProps(newProps) {
+    clearInterval(this.refreshInterval)
+    this.refreshInterval = setInterval(() => this.forceFetchComments(), Math.max((this.props.refreshInterval || 60) * 1000, 5000))
+    this.commentsClient = new CommentsClient(newProps.accessToken, newProps.resourceUri)
     if (newProps.resourceUri !== this.props.resourceUri) {
       this.setState({
         failed: false,
         commentsIds: []
-      }, () => this.forceFetchComments());
+      }, () => this.forceFetchComments())
     }
   }
 
-  onInputChange (event, newValue, newPlainTextValue, mentions) {
-      this.setState({commentToAdd: newValue});
+  onInputChange(event, newValue, newPlainTextValue, mentions) {
+    this.setState({commentToAdd: newValue})
   }
 
-  addComment (e) {
-    this.postComment(this.state.commentToAdd);
+  addComment(e) {
+    this.postComment(this.state.commentToAdd)
   }
 
-  fetchComments (isVisible) {
+  fetchComments(isVisible) {
     this.setState({
       visible: isVisible
-    });
+    })
     if (isVisible && this.props.resourceUri) {
-      this.forceFetchComments();
+      this.forceFetchComments()
     }
   }
 
-  forceFetchComments () {
+  forceFetchComments() {
     if (!this._ismounted) {
-      return;
+      return
     }
 
     this.setState({
       loading: true,
       failed: false
-    });
-    let currentClient = this.commentsClient;
+    })
+    let currentClient = this.commentsClient
     currentClient.fetchComments().then(responseJson => {
       if (currentClient.resourceUri === this.props.resourceUri && this._ismounted) {
         this.setState({
@@ -106,94 +108,92 @@ export default class _Comments extends React.Component {
           failed: false,
           commentsIds: responseJson.sort((a, b) => {
             if (this.props.newestFirst === true) {
-              return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+              return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
             } else {
-              return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+              return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
             }
           }).map(c => c.id),
           commentObjects: responseJson.reduce((acc, curr) => {
-            acc[curr.id] = curr;
-            return acc;
+            acc[curr.id] = curr
+            return acc
           }, {})
-        });
+        })
       }
     }).catch(err => {
       if (currentClient.resourceUri === this.props.resourceUri) {
         this.setState({
           loading: false,
           failed: true
-        });
+        })
       }
-      console.error(err);
+      console.error(err)
     }).then(() => {
-      this.reportCommentCount();
-    });
+      this.reportCommentCount()
+    })
   }
 
-  postComment (comment) {
+  postComment(comment) {
     if (!comment || comment.length === 0) {
-      return;
+      return
     }
-    let tempId = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    let tempId = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)
     if (this.props.newestFirst) {
-      this.state.commentsIds.unshift(tempId);
+      this.state.commentsIds.unshift(tempId)
     } else {
-      this.state.commentsIds.push(tempId);
+      this.state.commentsIds.push(tempId)
     }
     this.setState({
       commentToAdd: '',
       commentsIds: this.state.commentsIds.slice(0),
       commentsObjects: Object.assign({[tempId]: {comment}}, this.state.commentObjects)
-    });
+    })
     return this.commentsClient.postComment(comment).then(() => this.fetchComments(this.state.visible)).then(() => {
-      this.reportCommentCount();
-    });
+      this.reportCommentCount()
+    })
   }
 
-  reportCommentCount () {
+  reportCommentCount() {
     if (this.props.commentCountRefreshed) {
-      this.props.commentCountRefreshed(this.state.commentsIds.length);
+      this.props.commentCountRefreshed(this.state.commentsIds.length)
     }
   }
 
   onAlertDismissed() {
-    fetch(`${CUSTOMIZR_URL}/v1/settings`, {
-      method: "POST",
+    fetch(`${CUSTOMIZR_URL}/v1/${this.customizrResource}/settings`, {
+      method: "PUT",
       headers: {
         'Authorization': `Bearer ${this.props.accessToken}`,
       },
       data: {
-        resourceId: this.customizrResource,
-        data: {
-          mentionsUsageNotification: {
-            alertDismissed: true
-          }
+        mentionsUsageNotification: {
+          alertDismissed: true
         }
       }
     })
   }
 
-  render () {
+  render() {
 
-    let encodedUri = encodeURIComponent(this.props.resourceUri);
-    let url = `${this.commentServiceUrl}/v0/resources/${encodedUri}/comments/`;
-    let comments = null;
+    let encodedUri = encodeURIComponent(this.props.resourceUri)
+    let url = `${this.commentServiceUrl}/v0/resources/${encodedUri}/comments/`
+    let comments = null
 
     if (!this.props.resourceUri) {
-      comments = <p>Incorrect component setup.</p>;
+      comments = <p>Incorrect component setup.</p>
     } else if (this.state.commentsIds.length > 0) {
       comments = this.state.commentsIds.map(
-        (id, index) => <Comment className={'comment ' + ((index % 2 === 0) ? 'comment-even' : 'comment-odd')} key={id} accessToken={this.props.accessToken}
-                                commentUri={url + id} comment={this.state.commentObjects[id]} editComments={this.props.editComments}/>);
+        (id, index) => <Comment className={'comment ' + ((index % 2 === 0) ? 'comment-even' : 'comment-odd')} key={id}
+          accessToken={this.props.accessToken}
+          commentUri={url + id} comment={this.state.commentObjects[id]} editComments={this.props.editComments}/>)
     } else if (this.state.loading) {
       comments = <div>
         <div className="inline-spinner"><Spinner size={20}/></div>
         <div className="inline-spinner">Retrieving comments.</div>
-      </div>;
+      </div>
     } else if (this.state.failed) {
-      comments = <p>Unable to retrieve comments.</p>;
+      comments = <p>Unable to retrieve comments.</p>
     } else {
-      comments = <p>No comments here yet.</p>;
+      comments = <p>No comments here yet.</p>
     }
 
     let addCommentBox = <div>
@@ -201,21 +201,26 @@ export default class _Comments extends React.Component {
         className={'comments_alert'}>
         <Alert
           type={"info"}
-          message={<p>Use <strong>@</strong> character to mention people. We will notify them via email about this comment thread.</p>}
+          message={<p>Use <strong>@</strong> character to mention people. We will notify them via email about this
+            comment thread.</p>}
           dismissible={true}
           dismissed={this.state.alertDismissed}
           onDismiss={this.onAlertDismissed.bind(this)}
         />
       </div>
       <div style={{display: 'table'}}>
-      <MentionsInput className="mentions mentions-min-height" value={this.state.commentToAdd} onChange={this.onInputChange.bind(this)}
-                     displayTransform={(id, display, type) => `@${display}`} allowSpaceInQuery={true}>
-        <Mention trigger="@"
-                 data={(search, callback) => { this.mentionsClient.fetchMatchingMentions(search).then(callback); }}
-        />
-      </MentionsInput>
-      <span className="input-group-btn" style={{display: 'table-cell'}}>
-          <button disabled={!this.props.resourceUri || this.state.commentToAdd.trim() === ''} onClick={this.addComment.bind(this)} className="btn btn-default">
+        <MentionsInput className="mentions mentions-min-height" value={this.state.commentToAdd}
+          onChange={this.onInputChange.bind(this)}
+          displayTransform={(id, display, type) => `@${display}`} allowSpaceInQuery={true}>
+          <Mention trigger="@"
+            data={(search, callback) => {
+              this.mentionsClient.fetchMatchingMentions(search).then(callback)
+            }}
+          />
+        </MentionsInput>
+        <span className="input-group-btn" style={{display: 'table-cell'}}>
+          <button disabled={!this.props.resourceUri || this.state.commentToAdd.trim() === ''}
+            onClick={this.addComment.bind(this)} className="btn btn-default">
             post
           </button>
       </span>
@@ -232,7 +237,7 @@ export default class _Comments extends React.Component {
           {!this.props.newestFirst ? addCommentBox : null}
         </div>
       </VisibilitySensor>
-    );
+    )
   }
 }
 
@@ -244,4 +249,4 @@ _Comments.propTypes = {
   refreshInterval: PropTypes.number,
   commentCountRefreshed: PropTypes.func,
   initialValue: PropTypes.string
-};
+}
