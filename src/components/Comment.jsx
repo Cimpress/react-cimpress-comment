3644c50141673451203c1c1e9c9721789ca01ad8
Ivan Stanishev <istanishev@cimpress.com>
2018-12-07 11:15:22 +0100

    Remove console.log

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -65,7 +65,6 @@ class Comment extends React.Component {
     fetchUserName(userId, stateToUpdate) {
         fetchUserName(this.props.accessToken, userId)
             .then((responseJson) => {
-                console.log(userId, responseJson);
                 this.safeSetState({
                     [stateToUpdate]: responseJson.profile.name,
                 });
